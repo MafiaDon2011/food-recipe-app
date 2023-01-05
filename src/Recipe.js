bbc333ace8bba59737423a6737327e4c8bb06022
MafiaDon2011 <wajidhussain1@live.com>
2023-01-05 16:04:05 +0000

    Completed the styling, and producing the results from the API

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import style from './recipe.module.css'
 
-export default function Recipe({title, calories, image}) {
+export default function Recipe({title, calories, image, ingredients}) {
   return (
-    <div>
+    <>
+    <div className={style.recipe}>
       <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} />
+      <ol>
+        {ingredients.map(ingredients => (
+            <li>{ingredients.text}</li>
+        ))}
+      </ol>
+      <p>Calories: {calories}</p>
+      <img className={style.image} src={image} />
     </div>
+    </>
   )
 }
