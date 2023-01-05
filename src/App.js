de578330f369d254c67bb2589de8028b3d853f8c
MafiaDon2011 <wajidhussain1@live.com>
2023-01-05 14:44:59 +0000

    made function that would request example data from edamam api for recipes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const APP_ID = "#"
+  const APP_KEY = "#" 
+
+  useEffect(() => {
+   getRecipes()
+  }, [])
+  
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  return(
+    <div className='App'>
+      <form className='search'>
+        <input className='search-bar' type="text" />
+        <button className='search-button' type='submit'>
+          Submit
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
